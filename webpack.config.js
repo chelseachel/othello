6d86b00b617a151c74e9a01b28cde1720d63853a
chelseachel <cheecheese@outlook.com>
2020-03-04 02:57:48 +0800

    upload

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+module.exports = {
+  entry: './script.js',
+  output: {
+    path: path.resolve(__dirname,'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+                'babel-preset-env'
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
